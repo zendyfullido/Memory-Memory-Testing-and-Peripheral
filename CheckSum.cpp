5c1cc1216d986af96c9bf587478d6a947dc069f1
zendyfullido <48266445+zendyfullido@users.noreply.github.com>
2022-10-29 23:42:42 +0800

    Add files via upload

--- CheckSum.cpp
+++ CheckSum.cpp
@@ -40,91 +40,91 @@ using namespace std;
  
 int main()
 {
-    char a[20],b[20];
-    char sum[20],complement[20];
+    char a [20], b[20];
+    char sum [20], complement[20];
     int i;
     
-cout<<"Enter first binary string\n";
-    cin>>a;
-    cout<<"Enter second binary string\n";
-    cin>>b;
+cout << "Enter first binary string:";
+    cin >> a;
+    cout <<"Enter second binary string:";
+    cin >> b;
     
-if(strlen(a)==strlen(b))
-    {
-        char carry='0';
-        int length=strlen(a);
+if (strlen(a) == strlen(b))
+{
+        char carry = '0';
+        int length = strlen(a);
         
-for(i=length-1;i>=0;i--)
+for (i=length-1; i>=0; i--)
         {
-            if(a[i]=='0' && b[i]=='0' && carry=='0')
+            if ( a[i] == '0' && b[i] == '0' && carry == '0')
             {
-                sum[i]='0';
-                carry='0';
+                sum[i] = '0';
+                carry = '0';
             }
-            else if(a[i]=='0' && b[i]=='0' && carry=='1')
+            else if (a[i] == '0' && b[i] == '0' && carry == '1')
             {
-                sum[i]='1';
-                carry='0';
+                sum [i] = '1';
+                carry = '0';
  
             }
-            else if(a[i]=='0' && b[i]=='1' && carry=='0')
+            else if (a[i] == '0' && b[i] == '1' && carry== '0')
             {
-                sum[i]='1';
-                carry='0';
+                sum[i]= '1';
+                carry= '0';
  
             }
-            else if(a[i]=='0' && b[i]=='1' && carry=='1')
+            else if (a[i]== '0' && b[i]== '1' && carry== '1')
             {
-                sum[i]='0';
-                carry='1';
+                sum [i] = '0';
+                carry = '1';
  
             }
-            else if(a[i]=='1' && b[i]=='0' && carry=='0')
+            else if (a[i] == '1' && b[i] == '0' && carry =='0')
             {
-                sum[i]='1';
-                carry='0';
+                sum[i] = '1';
+                carry = '0';
  
             }
-            else if(a[i]=='1' && b[i]=='0' && carry=='1')
+            else if (a[i] =='1' && b[i] =='0' && carry =='1')
             {
-                sum[i]='0';
-                carry='1';
+                sum[i] = '0';
+                carry = '1';
  
             }
-            else if(a[i]=='1' && b[i]=='1' && carry=='0')
+            else if (a[i] == '1' && b[i] == '1' && carry  == '0')
             {
-                sum[i]='0';
-                carry='1';
+                sum [i] = '0';
+                carry = '1';
  
             }
-            else if(a[i]=='1' && b[i]=='1' && carry=='1')
+            else if (a[i] == '1' && b[i] == '1' && carry == '1')
             {
-                sum[i]='1';
-                carry='1';
+                sum[i] = '1';
+                carry ='1';
  
             }
             else
                 break;
         }
-        cout<<"\nSum="<<carry<<sum;
+        cout << "\nSum=" << carry << sum;
  
-        for(i=0;i<length;i++)
+        for (i=0; i<length; i++)
         {
-            if(sum[i]=='0')
-                complement[i]='1';
+            if ( sum[i] == '0')
+                complement [i] = '1';
             else
-                complement[i]='0';
+                complement [i] = '0';
         }
         
-if(carry=='1')
-            carry='0';
+if (carry=='1')
+            carry ='0';
         else
-            carry='1';
+            carry ='1';
     
-    cout<<"\nChecksum="<<carry<<complement;
+    cout << "\nChecksum=" << carry << complement;
     }
     else
-        cout<<"\nWrong input strings";
+        cout << "\n Wrong Input String!" ;
         
     return 0;
 }
